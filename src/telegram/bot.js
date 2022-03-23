a4ed01b8705c16fcb9e75a11d22408d0e5871064
Yeray Lage <ylagef@gmail.com>
2022-03-23 15:35:12 +0100

    style: 🎨  format standard

--- src/telegram/bot.js
+++ src/telegram/bot.js
@@ -2,5 +2,8 @@ const { LISTENBOT, BOTTOKEN } = process.env
 
 const TelegramBot = require('node-telegram-bot-api')
 
-const bot = new TelegramBot(BOTTOKEN, { polling: LISTENBOT === '1', request: { family: 4 } })
+const bot = new TelegramBot(BOTTOKEN, {
+  polling: LISTENBOT === '1',
+  request: { family: 4 }
+})
 exports.bot = bot
